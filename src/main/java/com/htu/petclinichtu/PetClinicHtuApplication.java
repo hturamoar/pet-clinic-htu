594ecff3b566a4e421a928f5c18def860c33a34f
hturamoar <hturamoar@gmail.com>
2021-11-28 00:25:38 +0530

    Inital Project Commit
    Inital Project Commit

---
+++ src/main/java/com/htu/petclinichtu/PetClinicHtuApplication.java
@@ -0,0 +1,13 @@
+package com.htu.petclinichtu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicHtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicHtuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htu/petclinichtu/PetClinicHtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htu.petclinichtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicHtuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
