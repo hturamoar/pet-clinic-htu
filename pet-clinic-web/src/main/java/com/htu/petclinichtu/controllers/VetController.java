0762dd21c02438fbb3a8bc02995989a5624dde17
htu <hturamoar@gmail.com>
2021-12-06 16:15:25 +0530

    Added Vet list. Closes #15

--- pet-clinic-web/src/main/java/com/htu/petclinichtu/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/htu/petclinichtu/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.htu.petclinichtu.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.htu.petclinichtu.services.VetService;
 
 @Controller
 public class VetController {
 	
+private final VetService vetService;
+	
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
 	@GetMapping({"/vets","/vets/index","vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets",vetService.findAll());
 		return "vets/index";
 	}
 
