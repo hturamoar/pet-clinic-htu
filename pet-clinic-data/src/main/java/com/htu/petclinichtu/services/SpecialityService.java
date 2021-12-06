ceff266a95720b2687c77b84c34995a2603950c0
htu <hturamoar@gmail.com>
2021-12-06 22:44:41 +0530

    Added Map impl for Speciality Service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/htu/petclinichtu/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.htu.petclinichtu.services;
+
+import java.util.Set;
+
+import com.htu.petclinichtu.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/htu/petclinichtu/services/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package com.htu.petclinichtu.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.htu.petclinichtu.models.Speciality;
+import com.htu.petclinichtu.services.SpecialityService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality t) {
+		return super.save(t);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality t) {
+		super.delete(t);
+	}
+
+}
