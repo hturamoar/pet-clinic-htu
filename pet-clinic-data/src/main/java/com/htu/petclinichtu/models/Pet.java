9017f3e30fd69006db7fc92e881ba05e18bdd14d
htu <hturamoar@gmail.com>
2021-12-07 18:07:48 +0530

    Added Visit JPA, Closes #38

--- pet-clinic-data/src/main/java/com/htu/petclinichtu/models/Pet.java
+++ pet-clinic-data/src/main/java/com/htu/petclinichtu/models/Pet.java
@@ -1,11 +1,15 @@
 package com.htu.petclinichtu.models;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +26,9 @@ public class Pet extends BaseEntity{
 	private LocalDate birthDate;
 	@Column(name="name")
 	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 	
 	public PetType getPetType() {
 		return petType;
@@ -46,6 +53,12 @@ public class Pet extends BaseEntity{
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 	
 	
