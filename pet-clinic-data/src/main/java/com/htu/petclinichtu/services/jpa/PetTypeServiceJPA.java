6c99d3283b6facf01b4a9aee4cd7d101ad727fdf
htu <hturamoar@gmail.com>
2021-12-07 18:38:47 +0530

    Added PetType JPA Service. Closes #42

---
+++ pet-clinic-data/src/main/java/com/htu/petclinichtu/services/jpa/PetTypeServiceJPA.java
@@ -0,0 +1,47 @@
+package com.htu.petclinichtu.services.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.htu.petclinichtu.models.PetType;
+import com.htu.petclinichtu.repositories.PetRepository;
+import com.htu.petclinichtu.repositories.PetTypeRepository;
+import com.htu.petclinichtu.services.PetTypeService;
+
+public class PetTypeServiceJPA implements PetTypeService {
+	
+	private final PetTypeRepository petTypeRepository;
+
+	public PetTypeServiceJPA(PetTypeRepository petTypeRepository) {
+		super();
+		this.petTypeRepository = petTypeRepository;
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		Set<PetType> petTypes = new HashSet<PetType>();
+		petTypeRepository.findAll().forEach(petTypes::add);
+		return petTypes;
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return petTypeRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public PetType save(PetType t) {
+		return petTypeRepository.save(t);
+	}
+
+	@Override
+	public void delete(PetType t) {
+		petTypeRepository.delete(t);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petTypeRepository.deleteById(id);
+	}
+
+}
